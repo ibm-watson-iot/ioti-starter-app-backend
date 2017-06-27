5f71b531b18008a4f7022a5e49ce612e8e47bd04
Patrick Pircher <Patrick.Pircher@ibm.com>
2017-06-27 10:15:02 +0200

    add insurance dashboard websocket (#9)
    add loggging

--- src/actions/websocket-action.js
+++ src/actions/websocket-action.js
@@ -3,10 +3,30 @@
 const WebSocket = require('ws');
 const express = require('express');
 const Promise = require('bluebird');
+const _ = require('lodash');
+const logger = require('../utils/logger');
 
 module.exports = {
 
+  removeClient(ws) {
+    let key = Object.keys(this.clientConnection).find((k) => {
+      return this.clientConnection[k] === ws;
+    });
+    if (key) {
+      delete this.clientConnection[key];
+    }
+
+    key = Object.keys(this.insuranceConnections).find((k) => {
+      return this.insuranceConnections[k] === ws;
+    });
+    if (key) {
+      delete this.insuranceConnections[key];
+    }
+  },
+
   init: function(config) {
+
+    this.sendToInsurer = _.throttle(this.sendToInsurer, 1000);
 
     setTimeout(() => {
       if (!express.app.server) {
@@ -20,23 +40,28 @@ module.exports = {
       });
 
       this.clientConnection = {};
+      this.insuranceConnections = {};
 
       wss.on('connection', (ws) => {
         ws.on('message', (message) => {
           message = JSON.parse(message);
-          if (!this.clientConnection[message.userId]) {
+          logger.info('no-tid', 'websocket.message', message);
+          if (!this.clientConnection[message.userId] && !message.isInsurer) {
             // TODO: check message.userId, message.userKey/password
             this.clientConnection[message.userId] = ws;
+            logger.info('no-tid', 'websocket.message', 'set as client connection');
+          }
+
+          if (!this.insuranceConnections[message.userId] && message.isInsurer) {
+            // TODO: check message.userId, message.userKey/password
+            this.insuranceConnections[message.userId] = ws;
+            logger.info('no-tid', 'websocket.message', 'set as insurer connection');
           }
         });
 
         ws.on('close', () => {
-          const key = Object.keys(this.clientConnection).find((k) => {
-            return this.clientConnection[k] === ws;
-          });
-          if (key) {
-            delete this.clientConnection[key];
-          }
+          this.removeClient(ws);
+          logger.info('no-tid', 'websocket.close', 'removed connection');
         });
       });
 
@@ -52,12 +77,7 @@ module.exports = {
       setInterval(() => {
         wss.clients.forEach((ws) => {
           if (ws.isAlive === false) {
-            const key = Object.keys(this.clientConnection).find((k) => {
-              return this.clientConnection[k] === ws;
-            });
-            if (key) {
-              delete this.clientConnection[key];
-            }
+            this.removeClient(ws);
             return ws.terminate();
           }
 
@@ -68,17 +88,33 @@ module.exports = {
 
       this.wss = wss;
     }, 1000);
+  },
 
+  sendToInsurer: function(data) {
+    logger.info('no-tid', 'websocket.sendToInsurer', 'send to all insurers');
+    Object.keys(this.insuranceConnections).forEach((key) => {
+      const conn = this.insuranceConnections[key];
+      try {
+        conn.send(JSON.stringify({
+          event: 'new-hazard',
+          data: data,
+        }));
+      } catch (e) {}
+    });
   },
 
   performAction(data) {
-    return Promise.try(() => {
+    logger.info('no-tid', 'websocket.performAction', data);
+    try {
       if (data.userId && this.clientConnection[data.userId]) {
         this.clientConnection[data.userId].send(JSON.stringify({
           event: 'new-hazard',
           data: data,
         }));
       }
-    });
+    } catch (e) {}
+
+    this.sendToInsurer(data);
+    return Promise.resolve();
   }
 };
