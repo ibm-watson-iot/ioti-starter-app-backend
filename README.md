5d8b61739f00af38bfbc41951974c03016bc5a09
Bilal Al-Saeedi <bilal_alsaidi@yahoo.com>
2017-10-10 10:09:43 +0200

    Show more info in email action (#3)
    * Fix bug and readme
    * Update package-lock.json
    * Use pug email template and add more information to hazard email
    * Change style

--- README.md
+++ README.md
@@ -9,11 +9,19 @@
 
 #### How to run locally
 1. Create a config file under `config` directory with a name `config-dev.json`. You can use the template file `/config/config-template.json`.
-2. Create database and view in the database with the following command: `node init.js`
-3. Start the Nodejs application `node app.js`
+2. Start the Nodejs application `node app.js`
 
 #### How to run in a different environment
 1. Create a config file under `config` directory specific to your environment. Name of the config file must contain your environment's name. If your environment's name is `test`, you must create a config file with a name `config-test.json`. You can use the template file `/config/config-template.json`.
-2. Create database and view in the database with the following command: `node init.js`
-3. Set the following environment variable `APP_ENV` with your environment's name.
-4. Start the Nodejs application `node app.js`
+2. Set the following environment variable `APP_ENV` with your environment's name.
+3. Start the Nodejs application `node app.js`
+
+
+### Actions
+This repo has also some example actions that can be configured with IoT4i. The actions are listed below:
+
+- Email action which uses nodemailer to send emails. An html email template was designed using https://beefree.io/ and converted to [pug](https://pugjs.org). The template accepts hazard information like shieldId, locations and events from the email action. 
+- Phone call action which uses [twilio](https://www.twilio.com) to call phone number.
+- Send sms action which uses [twilio](https://www.twilio.com) to sends sms to a phone number.
+- Push action which uses bluemix push notification to send alerts.
+- Websocket action to send websocket notifications to browser. 
--- src/actions/email-action.js
+++ src/actions/email-action.js
@@ -3,6 +3,40 @@
 const Promise = require('bluebird');
 const nodemailer = require('nodemailer');
 const UserStore = require('../stores/UserStore');
+const pug = require('pug');
+const path = require('path');
+
+const compileEmailTemplate = pug.compileFile(path.join(__dirname, 'email.pug'), {
+  cache: true
+});
+
+function prepareEmailHtml(user, hazard) {
+  let locations = [];
+  let hazardTime = 'No hazard time found';
+  let eventsNumber = 0;
+  let hazardEvents = [];
+  if (hazard.rawEvents) {
+    locations = hazard.rawEvents.map((event) => {
+      return {
+        coordinates: event.location.geometry.coordinates
+      };
+    });
+    hazardTime = `${(hazard.rawEvents.length !== 0) ?
+      new Date(hazard.rawEvents[0].arrivedAtMH).toUTCString() : 'No hazard time found'}`;
+    eventsNumber = hazard.rawEvents.length;
+    hazardEvents = hazard.rawEvents.map(event => JSON.stringify(event.event, null, 4));
+  }
+  return compileEmailTemplate({
+    userName: user.name,
+    hazardTitle: `${(hazard.actionParams) ? hazard.actionParams.hazardTitle : ''}`,
+    hazardTime: hazardTime,
+    hazardDetails: `Shield Id: ${hazard.shieldId}`,
+    locationType: ' Coordinates [Lat,Lng]',
+    locations: JSON.stringify(locations, null, 4),
+    eventsNumber: eventsNumber,
+    hazardEvents: hazardEvents
+  });
+}
 
 module.exports = {
 
@@ -16,8 +50,8 @@ module.exports = {
       host: config.email.url,
       port: config.email.port * 1,
       auth: {
-        user: config.email.username,
-        pass: config.email.password
+        user: config.email.authUser,
+        pass: config.email.authPassword
       }
     });
     this.transporter.sendMail = Promise.promisify(this.transporter.sendMail);
@@ -31,7 +65,7 @@ module.exports = {
         to: toUser.email,
         subject: payload.actionParams.emailSubject,
         text: payload.actionParams.emailText,
-        html: payload.actionParams.emailHtml
+        html: prepareEmailHtml(toUser, payload)
       };
       // send mail with defined transport object
       return this.transporter.sendMail(mailOptions).then((info) => {
